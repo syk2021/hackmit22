b51b8a98e83da4d03618d48cc53976aadbf61e67
Sanskar Lamsal <lamsal9677@yahoo.com>
2022-10-02 00:44:18 -0400

    hover function

--- client/toolshare/src/components/ProductListing.js
+++ client/toolshare/src/components/ProductListing.js
@@ -10,11 +10,24 @@ export default function ProductListing(props){
         <div class="productcontainer">
             <img class="productimage" src="https://i.imgur.com/YUlU9co.jpeg" alt='abc'/>
             <div class="productoverlay">
-            <div onClick={() => setdisp2(true)}>Sanskar</div>
-            <br/>
-            {disp2 ? (<div onClick={() => setdisp3(true)} >Lamsal</div>):<></>}
-            <br/>
-            {disp3 ? (<div onClick = {() => setdisp3(false) & setdisp2(false)}>Hello</div>):<></>}
+            <div onClick={() => setdisp2(true)}>
+                Available for August
+                <br/>
+                <div>
+                <button id='dateButton'>1</button>
+                <button id='dateButton'>12</button>
+                <button id='dateButton'>21</button>
+                </div>
+            </div>
+            {disp2 ? (<div onClick={() => setdisp3(true)}>
+                <div>
+                <button id='dateButton2'>11:00 AM</button>
+                <button id='dateButton2'>11:30 AM</button>
+                </div>
+            </div>):<></>}
+            {disp3 ? (<div onClick = {() => setdisp3(false) & setdisp2(false)}>
+                <button id='dateButton3'>Confirm Registration</button>
+            </div>):<></>}
             </div>
         </div>
         <div id='productTitle'>
