5df6a2293871cf67f7b06ec3a8557b77f3ca6bad
Sanskar Lamsal <lamsal9677@yahoo.com>
2022-10-02 02:24:14 -0400

    add page

--- client/toolshare/src/App.js
+++ client/toolshare/src/App.js
@@ -2,7 +2,7 @@
 import HomePage from './pages/HomePage';
 import ItemPage from './pages/ItemPage';
 import ReservationsPage from './pages/ReservationsPage';
-import AddPage from './pages/AddPage';
+import AddPageCat from './pages/AddPageCat';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
@@ -16,7 +16,7 @@ function App() {
             <Route exact path = "/" element={<HomePage/>}/>
             <Route exact path = "/item" element={<ItemPage/>}/>
             <Route exact path ="/reservations" element={<ReservationsPage/>}/>
-            <Route exact path ="/add" element={<AddPage/>}/>
+            <Route exact path ="/add" element={<AddPageCat/>}/>
           </Routes>
     </>
   )
---
+++ client/toolshare/src/pages/AddPageCat.jsx
@@ -0,0 +1,20 @@
+import './pages-css/AddPageCat.css'
+
+const AddPageCat = () => {
+    return (
+        <div id='mainbodyaddpagecat'>
+            <div id='sharenewcat'>Share New Item</div>
+            <br/>
+            <div id='itemcat2'>Item Category</div>
+            <br/>
+            <div id='catboxes'>Lawn and garden</div>
+            <div id='catboxes'>Kitchen Appliances</div>
+            <div id='catboxes'>Power Tool</div>
+            <div id='catboxes'>Roof and ladders</div>
+            <div id='catboxes'>Generators</div>
+            <div id='catboxes'>Plumbing</div>
+        </div>
+    );
+};
+
+export default AddPageCat;
